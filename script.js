5c665b54057e2442dc47ce0bf40ad0fc67d75287
DmutroKhludov <136634445+DmutroKhludov@users.noreply.github.com>
2024-08-29 10:30:19 +0300

    додав код
    додав код

---
+++ script.js
@@ -0,0 +1,12 @@
+const findLongestWord = function (string) {
+    const word = string.split(" ")
+    let longesWord = word [0] 
+    
+    for let i = 0; i < word 
+  };
+  
+  console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+  
+  console.log(findLongestWord('Google do a roll')); // 'Google'
+  
+  console.log(findLongestWord('May the force be with you')); // 'force'
