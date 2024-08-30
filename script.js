440ec230b0845c902dd7a3bd7eef769a9636fd8d
DmutroKhludov <136634445+DmutroKhludov@users.noreply.github.com>
2024-08-30 17:40:54 +0300

    код

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-const findLongestWord = function (string) {
-    const word = string.split(" ")
-    let longesWord = word [0] 
-    
-    for let i = 0; i < word 
-  };
-  
-  console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
-  
-  console.log(findLongestWord('Google do a roll')); // 'Google'
-  
-  console.log(findLongestWord('May the force be with you')); // 'force'
+1
+const bankAccount = {
+    ownerName: "Дмитро",
+    accountNumber: "123456789",
+    balance: 10000,
+
+    deposit: function () {
+        const amount = parseFloat(document.getElementById("amount").value);
+        if (isNaN(amount) || amount <= 0) {
+            document.getElementById("message").textContent = "Помилково введена сума";
+            return;
+        }
+        this.balance += amount;
+        document.getElementById("message").textContent = `Рахунок поповнено на ${amount}грн.`;
+        updateBalance();
+    },
+
+    withdraw: function () {
+        const amount = parseFloat(document.getElementById("amount").value);
+        if (isNaN(amount) || amount <= 0) {
+            document.getElementById("message").textContent = "Помилково введена сума";
+            return;
+        }
+
+        if (amount > this.balance) {
+            document.getElementById("message").textContent = "Недостатньо коштів";
+            return;
+        }
+
+        this.balance -= amount;
+        document.getElementById("message").textContent = `Знято ${amount}грн.`;
+        updateBalance();
+    }
+};
+
+function updateBalance() {
+    document.getElementById("ownerName").textContent = bankAccount.ownerName;
+    document.getElementById("accountNumber").textContent = bankAccount.accountNumber;
+    document.getElementById("balance").textContent = bankAccount.balance;
+}
+
+updateBalance();
